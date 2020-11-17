5bb3c2f2667e11cf0d7219a5891ee6fa91911a01
Evisari Riyanto <evisari111012@gmail.com>
2020-11-16 21:02:48 -0800

    added more pictures

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -6,6 +6,8 @@ import img4 from "../../images/massage-mothersday2.jpg";
 import img5 from "../../images/massage-office.jpg";
 import img6 from "../../images/massage-sushi2.jpg";
 import img7 from "../../images/home-massage2.png";
+import img8 from "../../images/home-massageedit.png";
+import img9 from "../../images/nes-massage.jpg";
 import Nav from "../Nav/Nav";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
@@ -15,13 +17,15 @@ export default function Gallery() {
     <div className="gallery">
       <Nav />
       <Header />
-      <img src={img1} alt="massage with dtla view" />
-      <img src={img2} alt="massage with dtla view" />
+      <img src={img1} alt="chair massage at a dog birthday party" />
+      <img src={img2} alt="massage at home" />
       <img src={img3} alt="massage with dtla view" />
-      <img src={img4} alt="massage with dtla view" />
-      <img src={img5} alt="massage with dtla view" />
-      <img src={img6} alt="massage with dtla view" />
-      <img src={img7} alt="massage with dtla view" />
+      <img src={img4} alt="chair massage for a mother's day gift" />
+      <img src={img5} alt="chair massage at an office" />
+      <img src={img6} alt="chair massage at a dog birthday party" />
+      <img src={img7} alt="massage at home" />
+      <img src={img8} alt="massage at home" />
+      <img src={img9} alt="nes massage" />
       <Footer />
     </div>
   );
